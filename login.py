a0d0f63f8827d4098b6f5cea52c51e1116be8b59
JonBits <112587370+JonathanIglesias@users.noreply.github.com>
2024-04-05 19:24:42 -0400

    Temp Update

--- login.py
+++ login.py
@@ -1,14 +1,40 @@
 import streamlit as st
+import duo_universal
+from streamlit.source_util import get_pages
+from streamlit_extras.switch_page_button import switch_page
 from st_login_form import login_form
+from duo_universal.client import Client, DuoException
+import streamlit.components.v1 as components
+import webbrowser
+from flask import Flask, request, redirect, session, render_template
 
-#Test
+def TFA(username):
+  duo_client = Client(
+      client_id="DIF2XDE121PIAYO9OXRF",
+      client_secret="5rQvC1JlKncpM03DuyvL64NnO4XDPvjDeNuoGbPq",
+      host="api-5f0df9c4.duosecurity.com",
+      redirect_uri= get_pages("main"),
+  )
+  try:
+    duo_client.health_check()
+  except DuoException:
+    st.error("Duo health check failed")
+  state = duo_client.generate_state()
+  #flask.session['state'] = state
+  prompt_uri = duo_client.create_auth_url(username, state)
+  components.iframe(prompt_uri)
+
 
 client = login_form()
 
 if st.session_state["authenticated"]:
-    if st.session_state["username"]:
-        st.success(f"Welcome {st.session_state['username']}")
-    else:
-        st.success("Welcome guest")
+  if st.session_state["username"]:
+    st.success(f"Welcome {st.session_state['username']}")
+    TFA(st.session_state["username"])
+  else:
+    st.success("Welcome guest")
+
 else:
-    st.error("Not authenticated")
+  st.error("Not authenticated")
+
+st.sidebar.success("Select a page")
