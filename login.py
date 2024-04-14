abbb18c9024c9282b64da0a91d68b6841f913243
JonBits <112587370+JonathanIglesias@users.noreply.github.com>
2024-04-14 20:57:01 +0000

    Code updated to be compatible with mysql, the SiddhantSadangi/st_login_form code had to be modified to meet the requirements

--- login.py
+++ login.py
@@ -1,11 +1,8 @@
-import webbrowser
 import streamlit as st
-import st_pages
-from streamlit.components.v1 import components, html
+import authn
 from duo_universal.client import Client, DuoException
-from streamlit_extras.switch_page_button import switch_page
 from streamlit_javascript import st_javascript
-from st_login_form import login_form
+
 
 def nav_to(url):
   nav_script = """
@@ -15,9 +12,9 @@ def nav_to(url):
 
 def TFA(username):
   duo_client = Client(
-      client_id="DIF2XDE121PIAYO9OXRF",
-      client_secret="5rQvC1JlKncpM03DuyvL64NnO4XDPvjDeNuoGbPq",
-      host="api-5f0df9c4.duosecurity.com",
+      client_id="",
+      client_secret="",
+      host="",
       redirect_uri= st_javascript("window.location.origin") + "/main"
   )
   try:
@@ -26,10 +23,7 @@ def TFA(username):
     st.error("Duo health check failed")
   state = duo_client.generate_state()
   prompt_uri = duo_client.create_auth_url(username, state)
-  #components.iframe(prompt_uri,700,700)
-  #st_javascript("window.location.replace(prompt_uri)")
-  #webbrowser.open(prompt_uri)
-  #open_page(prompt_uri)
+
   nav_to(prompt_uri)
 
 st.set_page_config(initial_sidebar_state="collapsed")
@@ -48,7 +42,7 @@ st.markdown(
 placeholder = st.empty()
 
 with placeholder.container():
-  client = login_form()
+  authn.login_form()
 
 if st.session_state["authenticated"]:
   if st.session_state["username"]:
